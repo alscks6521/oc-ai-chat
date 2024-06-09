e744982cdef5c02c1201f97c41c9c524cea8c6ec
Minsung <alscks6521@naver.com>
2024-06-09 16:37:08 +0900

    update

--- lib/screens/nutritional_dialog.dart
+++ lib/screens/nutritional_dialog.dart
@@ -14,16 +14,33 @@ class NutritionalContainer extends StatelessWidget {
           builder: (BuildContext context) {
             return SingleChildScrollView(
               child: AlertDialog(
-                title: Text(nutritionalItem['PRDLST_NM']),
+                shadowColor: Colors.transparent,
+                backgroundColor: Colors.white,
+                title: Text(
+                  nutritionalItem['PRDLST_NM'],
+                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
                 content: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    const Text(
+                      '주된 기능성:',
+                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                    ),
                     Text(
-                      '주된 기능성: ${nutritionalItem["PRIMARY_FNCLTY"]}',
+                      nutritionalItem["PRIMARY_FNCLTY"],
                       style: const TextStyle(fontSize: 17),
                     ),
-                    Text(
-                      '섭취시 주의사항: ${nutritionalItem['IFTKN_ATNT_MATR_CN']}',
+                    const SizedBox(
+                      height: 20,
                     ),
+                    const Text(
+                      '섭취시 주의사항:',
+                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      nutritionalItem['IFTKN_ATNT_MATR_CN'],
+                    )
                   ],
                 ),
                 actions: [
@@ -45,12 +62,18 @@ class NutritionalContainer extends StatelessWidget {
         height: 90,
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(
+            color: const Color(0xFF88B0FF),
+          ),
         ),
         child: Center(
           child: Text(
             nutritionalItem['PRDLST_NM'],
-            style: const TextStyle(fontSize: 18),
+            style: const TextStyle(fontSize: 17),
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1, // 필요한 경우 여러 줄로 설정할 수도 있습니다.
           ),
         ),
       ),
